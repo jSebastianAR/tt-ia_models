949e04400e44d9615c69f941ef085709f6a1298d
Jorge Reus <j-g1996@live.com>
2020-11-28 20:16:27 -0600

    Refactor de modelos

--- Models/ARIMA/arima_wheather.py
+++
@@ -1,5 +0,0 @@
-from ...shared import csv_utils
-import pandas as pd
-
-data = csv_utils.get_csv_data('/home/jsebastian-ar/Documentos/git-repos/tt-ia_models/Datasets/wheather-dataset/14002-ACATLAN DE JUAREZ.csv')
-print(data)
